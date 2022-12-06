a37f831534a4b9557f64fbde70a59e062105de4e
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2022-12-06 23:40:42 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,18 +3,18 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 12717501
+    API_HASH = "33bbce1e2706139eeda12cfd934e2f4f"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "¢αяιηg кιℓℓєя"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "Your value"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "5782906525:AAFpI1Q_mNj9mcfGSy43nexfrNLtAVooWtM"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -1001885198385
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
@@ -23,3 +23,4 @@ class Development(Config):
     EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
     # if you need badcat plugins set "True"
     BADCAT = "False"
+    ALIVE_PIC = "https://telegra.ph//file/d2cc42f5b6bd3803bde52.jpg"
